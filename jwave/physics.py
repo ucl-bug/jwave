bugfix old derivative calls + black

jwave/physics.py
@@ -153,9 +153,9 @@ def heterogeneous_laplacian(
     gamma, _ = _get_gamma_functions(grid, medium, omega, sigma_max)
 
     def derivative(field, ax):
-        return spectral.derivative(
-            field, grid, 0, ax, degree=1
-        )  # 0 is for "unstaggered"
+        return spectral.derivative(field, grid, 0, ax, degree=1)(
+            field
+        )  # 0 is for "unstaggered" # TODO: the function constructor should be outside
 
     def lapl(field, cmap):
         axis = list(range(field.ndim))
@@ -405,7 +405,7 @@ def get_helmholtz_operator_general(
     h_laplacian = heterogeneous_laplacian(grid, medium, omega)
 
     def helmholtz_operator(x, omega, medium):
-        return medium.density*h_laplacian(x, 1.0 / medium.density) + x * (
+        return medium.density * h_laplacian(x, 1.0 / medium.density) + x * (
             ((1 + 1j * medium.attenuation) * (omega / medium.sound_speed)) ** 2
         )
 
@@ -508,6 +508,7 @@ def solve_helmholtz(
 
     return field
 
+
 def velocity_update_fun(sample_input, grid):
     axes = nparange(-len(grid.N), 0, 1)
     deriv = spectral.derivative_with_k_op(sample_input, grid, -1, axes)
@@ -516,15 +517,20 @@ def velocity_update_fun(sample_input, grid):
         p = c_sq * jnp.sum(rho, 0)
         dp = deriv(p)
         return -dp / rho_0
+
     return d_velocity_dt
+
 
 def density_update_fun(sample_input, grid):
     axes = nparange(-len(grid.N), 0, 1)
-    deriv_funcs = [spectral.derivative_with_k_op(sample_input, grid, 1, ax) for ax in axes]
+    deriv_funcs = [
+        spectral.derivative_with_k_op(sample_input, grid, 1, ax) for ax in axes
+    ]
 
     def d_density_dt(u, rho_0):
-        diag_grad_u = jnp.stack([f(u[ax]) for f,ax in zip(deriv_funcs,axes)])
-        return - rho_0*diag_grad_u
+        diag_grad_u = jnp.stack([f(u[ax]) for f, ax in zip(deriv_funcs, axes)])
+        return -rho_0 * diag_grad_u
+
     return d_density_dt
 
 
@@ -538,7 +544,7 @@ def simulate_wave_propagation(
     output_t_axis=None,
     backprop=False,
     guess=None,
-    checkpoint=False
+    checkpoint=False,
 ):
     """Simulates wave propagation
 
@@ -607,8 +613,8 @@ def simulate_wave_propagation(
         signals = source_signals[:, idx] / len(N)
         src = src.at[sources.positions].add(signals)
         return src
-    
-    sample_input =get_src_map(0)
+
+    sample_input = get_src_map(0)
     d_velocity_dt = velocity_update_fun(sample_input, grid)
     d_density_dt = density_update_fun(sample_input, grid)
 
@@ -625,7 +631,7 @@ def simulate_wave_propagation(
         idx = (t / dt).round().astype(jnp.int32)
         src = get_src_map(idx)
 
-        return rho_update + src 
+        return rho_update + src
 
     # Checkpoint functions to save memory if requested
     fields = ode.generalized_semi_implicit_euler(

jwave/signal_processing.py
@@ -60,6 +60,7 @@ def gaussian_window(
     """
     return signal * jnp.exp(-((time - mu) ** 2) / sigma ** 2)
 
+
 def smoothing_filter(sample_input) -> jnp.ndarray:
     # Constructs the filter
     dimensions = sample_input.shape
@@ -74,20 +75,25 @@ def smoothing_filter(sample_input) -> jnp.ndarray:
             filter = jnp.fft.fftshift(jnp.outer(*axis))
         elif len(axis) == 3:
             filter_2d = jnp.outer(*axis[1:])
-            third_component = jnp.expand_dims(jnp.expand_dims(axis[0], 1),2)
-            filter = third_component*filter_2d
+            third_component = jnp.expand_dims(jnp.expand_dims(axis[0], 1), 2)
+            filter = third_component * filter_2d
 
     # Different filtering functions for real and complex data
     if sample_input.dtype != jnp.complex64 or sample_input.dtype != jnp.complex128:
         Fx = eval_shape(jnp.fft.rfft, sample_input)
-        filter = filter[...,:Fx.shape[-1]]
+        filter = filter[..., : Fx.shape[-1]]
+
         def smooth_fun(x):
             print(x.shape, jnp.fft.rfftn(x).shape, filter.shape)
             return jnp.fft.irfftn(filter * jnp.fft.rfftn(x)).real
+
     else:
+
         def smooth_fun(x):
             return jnp.fft.ifftn(filter * jnp.fft.fftn(x)).real
+
     return smooth_fun
+
 
 def smooth(x: jnp.ndarray) -> jnp.ndarray:
     """Smooths a  n-dimensioanl signal by multiplying its
@@ -111,8 +117,8 @@ def smooth(x: jnp.ndarray) -> jnp.ndarray:
             filter = jnp.fft.fftshift(jnp.outer(*axis))
         elif len(axis) == 3:
             filter_2d = jnp.outer(*axis[1:])
-            third_component = jnp.expand_dims(jnp.expand_dims(axis[0], 1),2)
-            filter = third_component*filter_2d
+            third_component = jnp.expand_dims(jnp.expand_dims(axis[0], 1), 2)
+            filter = third_component * filter_2d
     return jnp.fft.ifftn(filter * jnp.fft.fftn(x)).real
 
 

jwave/spectral.py
@@ -7,9 +7,7 @@ from jwave import geometry
 # TODO: speed tests
 
 
-def diag_nabla_with_k_op(
-    x: jnp.ndarray, grid: geometry.kGrid, staggered: int
-):
+def diag_nabla_with_k_op(x: jnp.ndarray, grid: geometry.kGrid, staggered: int):
     r = []
     for i in range(x.shape[0]):
         r.append(derivative_with_k_op(x[i], grid, staggered, i))
@@ -64,16 +62,22 @@ def derivative_with_k_op(x, grid, staggered, axis):
     if x.dtype != jnp.complex64 or x.dtype != jnp.complex128:
         # Use real fft
         Fx = eval_shape(lambda x: jnp.fft.rfftn(x, axes=domain_axes), x)
-        K = K[axis,:Fx.shape[0]]
+        K = K[axis, : Fx.shape[0]]
+
         def deriv_fun(x):
             return jnp.fft.irfftn(
-                K * jnp.fft.rfftn(x, axes=domain_axes), 
-                axes=domain_axes
+                K * jnp.fft.rfftn(x, axes=domain_axes), axes=domain_axes
             )
+
     else:
+
         def deriv_fun(x):
-            return jnp.fft.ifftn(K[axis] * jnp.fft.fftn(x, axes=domain_axes), axes=domain_axes)
+            return jnp.fft.ifftn(
+                K[axis] * jnp.fft.fftn(x, axes=domain_axes), axes=domain_axes
+            )
+
     return deriv_fun
+
 
 """
 # VJP rule ready, but crashes with linear_transpose() required
@@ -99,9 +103,6 @@ _derivative_with_k_op.defvjp(
 
 
 def plain_derivative(x, grid, staggered, axis, degree):
-    # Work on last axis for elementwise product broadcasting
-    x = jnp.moveaxis(x, axis, -1)
-
     # Spectral derivative
     if staggered == 0:
         k = 1j * grid.k_vec[axis]
@@ -110,11 +111,31 @@ def plain_derivative(x, grid, staggered, axis, degree):
     elif staggered == 1:
         k = 1j * grid.k_staggered["forward"][axis]
 
-    dx = _derivative_algorithm_last_axis(x, k, degree)
+    k = k ** degree
 
-    # Back to original axis
-    kx = jnp.moveaxis(dx, -1, axis)
-    return kx
+    if not ("complex" in str(x.dtype)):
+        ffts = [jnp.fft.rfft, jnp.fft.irfft]
+        Fx = eval_shape(lambda x: ffts[0](x, axis=-1), x)
+        k = k[: Fx.shape[-1]]
+    else:
+        ffts = [jnp.fft.fft, jnp.fft.ifft]
+
+    # Constructs derivative operator
+    def deriv_fun(
+        x,
+    ):  # TODO: use named axis? See https://jax.readthedocs.io/en/latest/notebooks/xmap_tutorial.html
+        # Work on last axis for elementwise product broadcasting
+        x = jnp.moveaxis(x, axis, -1)
+
+        # Use real or complex fft
+        Fx = ffts[0](x, axis=-1)
+        dx = ffts[1](k * Fx, axis=-1)
+
+        # Back to original axis
+        dx = jnp.moveaxis(dx, -1, axis)
+        return dx
+
+    return deriv_fun
 
 
 # @partial(jax.custom_vjp, nondiff_argnums=(2,3))
@@ -124,7 +145,7 @@ def _derivative_algorithm_last_axis(x, k, degree):
     if x.dtype != jnp.complex64 or x.dtype != jnp.complex128:
         # Use real fft
         Fx = jnp.fft.rfft(x, axis=-1)
-        k = k[:Fx.shape[-1]]
+        k = k[: Fx.shape[-1]]
         dx = jnp.fft.irfft(k * Fx, axis=-1)
     else:
         Fx = jnp.fft.fft(x, axis=-1)

tests/spectral_test.py
@@ -21,7 +21,8 @@ def test_real_derivative():
     )
     reference_da = jnp.array([-1.3090, 3.1602, 0, -3.1602, 1.3090, -0.5422, 0, 0.5422])
     grid = geometry.kGrid.make_grid(N, dx)
-    da = spectral.derivative(a, grid, 0, "real", 0)
+    da = spectral.derivative(a, grid, 0, 0)(a)
+    print(da.shape, reference_da.shape)
 
     assert_pytree_isclose(da, reference_da, relative_precision=1e-3, abs_precision=1e-4)
 
@@ -57,7 +58,7 @@ def test_complex_derivative():
 
     # Check
     grid = geometry.kGrid.make_grid(N, dx)
-    da = spectral.derivative(a, grid, 0, "complex", 0)
+    da = spectral.derivative(a, grid, 0, 0)(a)
     assert_pytree_isclose(da, reference_da, relative_precision=1e-3, abs_precision=1e-4)
 
 
@@ -74,7 +75,7 @@ def test_derivative_adjoint():
     grid = geometry.kGrid.make_grid(N, dx)
 
     def d(x):
-        return spectral.derivative(x, grid, 0, "real", 0, degree=2)
+        return spectral.derivative(x, grid, 0, 0, degree=2)(x)
 
     _, d_adj_raw = jax.vjp(d, b)
 
@@ -112,7 +113,7 @@ def test_derivative_with_kspaceop_adjoint():
     grid = grid.apply_kspace_operator(jnp.amin(medium.sound_speed), time_array.dt)
 
     def d(x):
-        return spectral.derivative(x, grid, 0, "real", 0, kspace_op=True)
+        return spectral.derivative(x, grid, 0, 0, kspace_op=True)(x)
 
     _, d_adj_raw = jax.vjp(d, jnp.zeros_like(b))
 