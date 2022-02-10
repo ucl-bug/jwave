475f928c9cc2be3f6f709b6905982798d7e92b5f
Antonio Stanziola <stanziola.antonio@gmail.com>
2022-02-10 18:44:18 +0000

    kspace op and heterog density with linear interpolation shift

--- jwave/acoustics/time_varying.py
+++ jwave/acoustics/time_varying.py
@@ -1,33 +1,49 @@
-from typing import Sequence
+from typing import Union
 
 import jax
 import numpy as np
 from jax import numpy as jnp
 from jaxdf import Field, operator
-from jaxdf.discretization import FiniteDifferences, FourierSeries, OnGrid
+from jaxdf.discretization import FourierSeries, OnGrid
 from jaxdf.operators import (
-    compose,
     diag_jacobian,
-    gradient,
+    functional,
     shift_operator,
     sum_over_dims,
 )
-from jaxdf.operators.differential import _get_ffts
 
-from jwave.geometry import Medium, TimeAxis
+from jwave.geometry import Medium, MediumObject, TimeAxis
 from jwave.signal_processing import smooth
 
 from .pml import td_pml_on_grid
 
 
-@operator
-def momentum_conservation_rhs(
-  p: Field,
-  rho0: Field,
-  params = None
-) -> Field:
-  return - gradient(p) / rho0
+def _get_kspace_op(field, c_ref, dt):
+  # Get the frequency axis manually, since we
+  # are nor using the rFFT
+  # TODO: Implement operators with rFFT
+  def f(N, dx):
+    return jnp.fft.fftfreq(N, dx) * 2 * jnp.pi
+  k_vec = [f(n, delta) for n, delta in zip(field.domain.N, field.domain.dx)]
 
+  # Building k-space operator
+  K = jnp.stack(jnp.meshgrid(*k_vec, indexing='ij'))
+  k_magnitude = jnp.sqrt(jnp.sum(K ** 2, 0))
+  k_space_op = jnp.sinc(c_ref * k_magnitude * dt / (2 * jnp.pi))
+  parameters = {"k_vec": k_vec, "k_space_op": k_space_op}
+  return parameters
+
+def _shift_rho_for_fourier(rho0, direction, dx):
+  if isinstance(rho0, OnGrid):
+    rho0_params = rho0.params[...,0]
+    def linear_interp(u, axis):
+      return 0.5*(jnp.roll(u, direction, axis) + u)
+    rho0 = jnp.stack([linear_interp(rho0_params, n) for n in range(rho0.ndim)], axis=-1)
+  elif isinstance(rho0, Field):
+    rho0 = shift_operator(rho0, direction*dx)
+  else:
+    pass
+  return rho0
 
 @operator
 def momentum_conservation_rhs(
@@ -52,69 +68,48 @@ def momentum_conservation_rhs(
   rho0 = shift_operator(rho0, dx)
   return -dp / rho0, params
 
-@operator
-def momentum_conservation_rhs(
-  p: FourierSeries,
-  rho0: object,
-  params = None
-) -> FourierSeries:
-
-  if params == None:
-    params = {'k_vec': p._freq_axis}
-
-  ffts = _get_ffts(p)
-  k_vec = params['k_vec']
-  u = p.params[...,0]
-
-  def single_grad(axis, u):
-    u = jnp.moveaxis(u, axis, -1)
-    Fx = ffts[0](u, axis=-1)
-    iku = 1j * Fx * k_vec[axis]
-    du = ffts[1](iku, axis=-1, n=u.shape[-1])
-    return jnp.moveaxis(du, -1, axis)
-
-  partial_derivatives = []
-  for i in range(p.ndim):
-    partial_derivatives.append(single_grad(i, u))
-  dp = p.replace_params(jnp.stack(partial_derivatives, axis=-1))
-  update = -dp / rho0
-  return update, params
 
 @operator
 def momentum_conservation_rhs(
   p: FourierSeries,
-  rho0: FiniteDifferences,
+  u: FourierSeries,
+  medium: Medium,
+  c_ref,
+  dt,
   params = None
 ) -> FourierSeries:
 
   if params == None:
-    params = {'k_vec': p._freq_axis}
+    params = _get_kspace_op(p, c_ref, dt)
+
+  dx = np.asarray(u.domain.dx)
+  direction = 1
 
   # Shift rho
-  rho0_params = rho0.params[...,0]
-  def linear_interp(u, axis):
-    return 0.5*(jnp.roll(u, 1, axis) + u)
-  rho0 = jnp.stack([linear_interp(rho0_params, n) for n in range(rho0.ndim)], axis=-1)
+  rho0 = _shift_rho_for_fourier(medium.density, direction, dx)
 
   # Take a shifted gradient of the pressure
-  assert p.dims == 1 # Gradient only defined for scalar fields
-
-  ffts = _get_ffts(p)
   k_vec = params['k_vec']
-  u = p.params[...,0]
+  k_space_op = params['k_space_op']
 
-  def single_grad(axis, u):
-    u = jnp.moveaxis(u, axis, -1)
-    Fx = ffts[0](u, axis=-1)
-    iku = 1j * Fx * k_vec[axis]
-    du = ffts[1](iku, axis=-1, n=u.shape[-1])
-    return jnp.moveaxis(du, -1, axis)
+  shift_and_k_op = [
+    1j * k * jnp.exp(1j * k * direction * delta / 2)
+    for k, delta in zip(k_vec, dx)
+  ]
 
-  dp = jnp.stack([single_grad(i, u) for i in range(p.ndim)], axis=-1)
-  update = -dp / rho0
+  p_params = p.params[...,0]
+  Fu = jnp.fft.fftn(p_params)
+
+  def single_grad(axis):
+    Fx = jnp.moveaxis(Fu, axis, -1)
+    k_op = jnp.moveaxis(k_space_op, axis, -1)
+    iku = jnp.moveaxis(Fx * shift_and_k_op[axis] * k_op, -1, axis)
+    return jnp.fft.ifftn(iku).real
+
+  dp = jnp.stack([single_grad(i) for i in range(p.ndim)], axis=-1)
+  update = -p.replace_params(dp) / rho0
 
   return update, params
-
 
 @operator
 def mass_conservation_rhs(
@@ -151,18 +146,67 @@ def mass_conservation_rhs(
   return - rho0 * du + mass_source, params
 
 
+
+@operator
+def mass_conservation_rhs(
+  p: FourierSeries,
+  u: FourierSeries,
+  mass_source: object,
+  medium: Medium,
+  c_ref,
+  dt,
+  params = None
+) -> FourierSeries:
+
+  if params == None:
+    params = _get_kspace_op(p, c_ref, dt)
+
+  dx = np.asarray(u.domain.dx)
+  direction = -1
+
+  k_vec = params['k_vec']
+  k_space_op = params['k_space_op']
+  rho0 = medium.density
+
+  # Add shift to k vector
+  shift_and_k_op = [
+    1j * k * jnp.exp(1j * k * direction * delta / 2)
+    for k, delta in zip(k_vec, dx)
+  ]
+
+  def single_grad(axis, u):
+    Fu = jnp.fft.fftn(u)
+    Fx = jnp.moveaxis(Fu, axis, -1)
+    k_op = jnp.moveaxis(k_space_op, axis, -1)
+    iku = jnp.moveaxis(Fx * shift_and_k_op[axis] *  k_op, -1, axis)
+    return jnp.fft.ifftn(iku).real
+
+  du = jnp.stack([single_grad(i, u.params[...,i]) for i in range(p.ndim)], axis=-1)
+  update = -p.replace_params(du) * rho0 + mass_source / du.ndim
+
+  return update, params
+
 @operator
 def pressure_from_density(
   rho: Field,
-  c0: object,
+  medium: Medium,
   params = None
 ) -> Field:
   rho_sum = sum_over_dims(rho)
+  c0 = medium.sound_speed
   return (c0**2) * rho_sum, params
 
+# Custom type
+FourierOrScalars = Union[
+  MediumObject[object,object,FourierSeries],
+  MediumObject[object,FourierSeries,object],
+  MediumObject[FourierSeries,object,object],
+  MediumObject[object,object,object],
+]
 
+@operator
 def simulate_wave_propagation(
-  medium: Medium,
+  medium: FourierOrScalars,
   time_axis: TimeAxis,
   sources = None,
   sensors = None,
@@ -171,15 +215,15 @@ def simulate_wave_propagation(
   checkpoint: bool = False,
   params = None,
   smooth_initial = True,
-) -> Sequence[Field]:
+):
 
   # Default sensors simply return the fields
   if sensors is None:
-    sensors = lambda x: {'p': x['p']}
+    sensors = lambda x: x
 
   # Setup parameters
   if params == None:
-    c_ref = compose(medium.sound_speed)(jnp.amax)
+    c_ref = functional(medium.sound_speed)(jnp.amax)
     dt = time_axis.dt
 
     t = jnp.arange(0, time_axis.t_end + time_axis.dt, time_axis.dt)
@@ -191,7 +235,14 @@ def simulate_wave_propagation(
       pml = medium.sound_speed.replace_params(pml_grid)
     else:
       pml = FourierSeries(pml_grid, medium.domain)
-    params = {'pml': pml,'output_steps': output_steps}
+
+    # Get k-space operator
+    fourier = _get_kspace_op(pml, c_ref, dt)
+    params = {
+      'pml': pml,
+      'output_steps': output_steps,
+      'fourier': fourier,
+    }
 
   # Get parameters
   pml = params['pml']
@@ -219,8 +270,6 @@ def simulate_wave_propagation(
 
   # define functions to integrate
   fields = [p0, u0, rho]
-  rho0 = medium.density
-  c = medium.sound_speed
   alpha = params['pml']
   output_steps = params['output_steps']
 
@@ -231,14 +280,14 @@ def simulate_wave_propagation(
     else:
       mass_src_field = sources.on_grid(n)
 
-    du = momentum_conservation_rhs(p, rho0)
+    du = momentum_conservation_rhs(p, u, medium, c_ref, dt, params=params['fourier'])
     u = alpha*(alpha*u + dt * du)
 
-    drho = mass_conservation_rhs(u, rho0, mass_src_field)
+    drho = mass_conservation_rhs(p, u, mass_src_field, medium, c_ref, dt, params=params['fourier'])
     rho = alpha*(alpha*rho + dt * drho)
 
-    p = pressure_from_density(rho, c)
-    return [p,u, rho], sensors({'u': u, 'rho': rho, 'p': p})
+    p = pressure_from_density(rho, medium)
+    return [p, u, rho], sensors(p)
 
   if checkpoint:
     scan_fun = jax.checkpoint(scan_fun)
--- jwave/geometry.py
+++ jwave/geometry.py
@@ -8,263 +8,288 @@ from jax import numpy as jnp
 from jax.tree_util import register_pytree_node_class
 from jaxdf import Field
 from jaxdf.geometry import Domain
-from jaxdf.operators import compose
+from jaxdf.operators import functional
+from plum import parametric, type_of
 
 
 @register_pytree_node_class
 class Medium:
-    r"""
-    Medium structure
-    Attributes:
-      domain (Domain): domain of the medium
-      sound_speed (jnp.darray): speed of sound map, can be a scalar
-      density (jnp.ndarray): density map, can be a scalar
-      attenuation (jnp.ndarray): attenuation map, can be a scalar
-      pml_size (int): size of the PML layer in grid-points
+  r"""
+  Medium structure
+  Attributes:
+    domain (Domain): domain of the medium
+    sound_speed (jnp.darray): speed of sound map, can be a scalar
+    density (jnp.ndarray): density map, can be a scalar
+    attenuation (jnp.ndarray): attenuation map, can be a scalar
+    pml_size (int): size of the PML layer in grid-points
 
-    !!! example
-        ```python
-        N = (128,356)
-        medium = Medium(
-            sound_speed = jnp.ones(N),
-            density = jnp.ones(N),.
-            attenuation = 0.0,
-            pml_size = 15
-        )
-        ```
-    """
-    domain: Domain
-    sound_speed: Union[Number, Field] = 1.0
-    density: Union[Number, Field] = 1.0
-    attenuation: Union[Number, Field] = 0.0
-    pml_size: Number = 20
+  !!! example
+    ```python
+    N = (128,356)
+    medium = Medium(
+      sound_speed = jnp.ones(N),
+      density = jnp.ones(N),.
+      attenuation = 0.0,
+      pml_size = 15
+    )
+    ```
+  """
+  domain: Domain
+  sound_speed: Union[Number, Field] = 1.0
+  density: Union[Number, Field] = 1.0
+  attenuation: Union[Number, Field] = 0.0
+  pml_size: Number = 20
 
-    def __init__(
-        self, domain, sound_speed = 1.0, density=1.0, attenuation=0.0, pml_size=20
-    ):
-      # Check that all domains are the same
-      for field in [sound_speed, density, attenuation]:
-        if isinstance(field, Field):
-          assert domain == field.domain, "All domains must be the same"
+  def __init__(
+    self, domain, sound_speed = 1.0, density=1.0, attenuation=0.0, pml_size=20
+  ):
+    # Check that all domains are the same
+    for field in [sound_speed, density, attenuation]:
+      if isinstance(field, Field):
+        assert domain == field.domain, "All domains must be the same"
 
-      # Set the attributes
-      self.domain = domain
-      self.sound_speed = sound_speed
-      self.density = density
-      self.attenuation = attenuation
-      self.pml_size = pml_size
+    # Set the attributes
+    self.domain = domain
+    self.sound_speed = sound_speed
+    self.density = density
+    self.attenuation = attenuation
+    self.pml_size = pml_size
 
-    def tree_flatten(self):
-      children = (self.sound_speed, self.density, self.attenuation, self.pml_size)
-      aux = (self.domain,)
-      return (children, aux)
+  def tree_flatten(self):
+    children = (self.sound_speed, self.density, self.attenuation, self.pml_size)
+    aux = (self.domain,)
+    return (children, aux)
 
-    @classmethod
-    def tree_unflatten(cls, aux, children):
-      sound_speed, density, attenuation, pml_size = children
-      domain = aux[0]
-      a = cls(domain, sound_speed, density, attenuation, pml_size)
-      return a
+  @classmethod
+  def tree_unflatten(cls, aux, children):
+    sound_speed, density, attenuation, pml_size = children
+    domain = aux[0]
+    a = cls(domain, sound_speed, density, attenuation, pml_size)
+    return a
 
-    def __str__(self) -> str:
-        return self.__repr__()
+  def __str__(self) -> str:
+    return self.__repr__()
 
-    def __repr__(self) -> str:
-        def show_param(pname):
-            attr = getattr(self, pname)
-            return f'{pname}: ' + str(attr)
-        all_params = sorted(['domain', 'sound_speed','density','attenuation', 'pml_size'])
-        strings = list(map(lambda x: show_param(x), all_params))
-        return 'Medium:\n - ' + '\n - '.join(strings)
+  def __repr__(self) -> str:
+    def show_param(pname):
+      attr = getattr(self, pname)
+      return f'{pname}: ' + str(attr)
+    all_params = sorted(['domain', 'sound_speed','density','attenuation', 'pml_size'])
+    strings = list(map(lambda x: show_param(x), all_params))
+    return 'Medium:\n - ' + '\n - '.join(strings)
 
-def _points_on_circle(n, radius, centre, cast_int=True, angle=0.0):
-    angles = np.linspace(0, 2 * np.pi, n, endpoint=False)
-    x = (radius * np.cos(angles + angle) + centre[0]).tolist()
-    y = (radius * np.sin(angles + angle) + centre[1]).tolist()
-    if cast_int:
-        x = list(map(int, x))
-        y = list(map(int, y))
-    return x, y
 
+
+def _points_on_circle(n, radius, centre, cast_int=True, angle=0.0, max_angle=2*np.pi):
+  angles = np.linspace(0, max_angle, n, endpoint=False)
+  x = (radius * np.cos(angles + angle) + centre[0]).tolist()
+  y = (radius * np.sin(angles + angle) + centre[1]).tolist()
+  if cast_int:
+    x = list(map(int, x))
+    y = list(map(int, y))
+  return x, y
+
+@parametric(runtime_type_of=True)
+class MediumObject(Medium):
+  pass
+
+@type_of.dispatch
+def type_of(m: Medium):
+  return MediumObject[type(m.sound_speed), type(m.density), type(m.attenuation)]
 
 def _unit_fibonacci_sphere(samples=128):
-    # From https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere
-    points = []
-    phi = math.pi * (3.0 - math.sqrt(5.0))  # golden angle in radians
-    for i in range(samples):
-        y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
-        radius = math.sqrt(1 - y * y)  # radius at y
-        theta = phi * i  # golden angle increment
-        x = math.cos(theta) * radius
-        z = math.sin(theta) * radius
-        points.append((x, y, z))
-    return points
+  # From https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere
+  points = []
+  phi = math.pi * (3.0 - math.sqrt(5.0))  # golden angle in radians
+  for i in range(samples):
+    y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
+    radius = math.sqrt(1 - y * y)  # radius at y
+    theta = phi * i  # golden angle increment
+    x = math.cos(theta) * radius
+    z = math.sin(theta) * radius
+    points.append((x, y, z))
+  return points
 
 
 def _fibonacci_sphere(n, radius, centre, cast_int=True):
-    points = _unit_fibonacci_sphere(n)
-    points = np.array(points)
-    points = points * radius + centre
-    if cast_int:
-        points = points.astype(int)
-    return points[:, 0], points[:, 1], points[:, 2]
+  points = _unit_fibonacci_sphere(n)
+  points = np.array(points)
+  points = points * radius + centre
+  if cast_int:
+    points = points.astype(int)
+  return points[:, 0], points[:, 1], points[:, 2]
 
 
 def _circ_mask(N, radius, centre):
-    x, y = np.mgrid[0 : N[0], 0 : N[1]]
-    dist_from_centre = np.sqrt((x - centre[0]) ** 2 + (y - centre[1]) ** 2)
-    mask = (dist_from_centre < radius).astype(int)
-    return mask
+  x, y = np.mgrid[0 : N[0], 0 : N[1]]
+  dist_from_centre = np.sqrt((x - centre[0]) ** 2 + (y - centre[1]) ** 2)
+  mask = (dist_from_centre < radius).astype(int)
+  return mask
 
 
 def _sphere_mask(N, radius, centre):
-    x, y, z = np.mgrid[0 : N[0], 0 : N[1], 0 : N[2]]
-    dist_from_centre = np.sqrt(
-        (x - centre[0]) ** 2 + (y - centre[1]) ** 2 + (z - centre[2]) ** 2
-    )
-    mask = (dist_from_centre < radius).astype(int)
-    return mask
+  x, y, z = np.mgrid[0 : N[0], 0 : N[1], 0 : N[2]]
+  dist_from_centre = np.sqrt(
+    (x - centre[0]) ** 2 + (y - centre[1]) ** 2 + (z - centre[2]) ** 2
+  )
+  mask = (dist_from_centre < radius).astype(int)
+  return mask
 
 
 @dataclass
 class Sources:
-    r"""Sources structure
-    Attributes:
-        positions (Tuple[List[int]): source positions
-        signals (List[jnp.ndarray]): source signals
-    !!! example
-        ```python
-        x_pos = [10,20,30,40]
-        y_pos = [30,30,30,30]
-        signal = jnp.sin(jnp.linspace(0,10,100))
-        signals = jnp.stack([signal]*4)
-        sources = geometry.Source(positions=(x_pos, y_pos), signals=signals)
-        ```
+  r"""Sources structure
+  Attributes:
+    positions (Tuple[List[int]): source positions
+    signals (List[jnp.ndarray]): source signals
+  !!! example
+    ```python
+    x_pos = [10,20,30,40]
+    y_pos = [30,30,30,30]
+    signal = jnp.sin(jnp.linspace(0,10,100))
+    signals = jnp.stack([signal]*4)
+    sources = geometry.Source(positions=(x_pos, y_pos), signals=signals)
+    ```
+  """
+  positions: Tuple[np.ndarray]
+  signals: Tuple[jnp.ndarray]
+  dt: float
+  domain: Domain
+
+  def to_binary_mask(self, N):
+    r"""
+    Convert sources to binary mask
+    Args:
+      N (Tuple[int]): grid size
+
+    Returns:
+      jnp.ndarray: binary mask
     """
-    positions: Tuple[np.ndarray]
-    signals: Tuple[jnp.ndarray]
-    dt: float
-    domain: Domain
+    mask = jnp.zeros(N)
+    for i in range(len(self.positions[0])):
+      mask = mask.at[self.positions[0][i], self.positions[1][i]].set(1)
+    return mask > 0
 
-    def to_binary_mask(self, N):
-        r"""
-        Convert sources to binary mask
-        Args:
-            N (Tuple[int]): grid size
+  def on_grid(self, t):
 
-        Returns:
-            jnp.ndarray: binary mask
-        """
-        mask = jnp.zeros(N)
-        for i in range(len(self.positions[0])):
-            mask = mask.at[self.positions[0][i], self.positions[1][i]].set(1)
-        return mask > 0
+    src = jnp.zeros(self.domain.N)
+    if len(self.signals) == 0:
+      return src
 
-    def on_grid(self, t):
+    idx = (t / self.dt).round().astype(jnp.int32)
+    signals = self.signals[:, idx] / len(self.domain.N)
+    src = src.at[self.positions].add(signals)
+    return jnp.expand_dims(src, -1)
 
-        src = jnp.zeros(self.domain.N)
-        if len(self.signals) == 0:
-            return src
-
-        idx = (t / self.dt).round().astype(jnp.int32)
-        signals = self.signals[:, idx] / len(self.domain.N)
-        src = src.at[self.positions].add(signals)
-        return jnp.expand_dims(src, -1)
-
-    @staticmethod
-    def no_sources(domain):
-        return Sources(positions=([], []), signals=([]), dt=1.0, domain=domain)
+  @staticmethod
+  def no_sources(domain):
+    return Sources(positions=([], []), signals=([]), dt=1.0, domain=domain)
 
 
 @dataclass
 class TimeHarmonicSource:
-    r"""TimeHarmonicSource dataclass
+  r"""TimeHarmonicSource dataclass
 
-    Attributes:
-      domain (Domain): domain
-      amplitude (Field): The complex amplitude field of the sources
-      omega (float): The angular frequency of the sources
+  Attributes:
+    domain (Domain): domain
+    amplitude (Field): The complex amplitude field of the sources
+    omega (float): The angular frequency of the sources
 
+  """
+  amplitude: Field
+  omega: Union[float, Field]
+  domain: Domain
+
+  def on_grid(self, t=0.0):
+    r"""Returns the complex field corresponding to the
+    sources distribution at time $`t`$.
     """
-    amplitude: Field
-    omega: Union[float, Field]
-    domain: Domain
-
-    def on_grid(self, t=0.0):
-        r"""Returns the complex field corresponding to the
-        sources distribution at time $`t`$.
-        """
-        return self.amplitude * jnp.exp(1j * self.omega * t)
+    return self.amplitude * jnp.exp(1j * self.omega * t)
 
 
 @dataclass
 class Sensors:
-    r"""Sensors structure
-    Attributes:
-        positions (Tuple[List[int]]): sensors positions
+  r"""Sensors structure
+  Attributes:
+    positions (Tuple[List[int]]): sensors positions
 
-    !!! example
-        ```python
-        x_pos = [10,20,30,40]
-        y_pos = [30,30,30,30]
-        sensors = geometry.Sensors(positions=(x_pos, y_pos))
-        ```
+  !!! example
+    ```python
+    x_pos = [10,20,30,40]
+    y_pos = [30,30,30,30]
+    sensors = geometry.Sensors(positions=(x_pos, y_pos))
+    ```
+  """
+
+  positions: Tuple[tuple]
+
+  def to_binary_mask(self, N):
+    r"""
+    Convert sensors to binary mask
+    Args:
+      N (Tuple[int]): grid size
+
+    Returns:
+      jnp.ndarray: binary mask
     """
+    mask = jnp.zeros(N)
+    for i in range(len(self.positions[0])):
+      mask = mask.at[self.positions[0][i], self.positions[1][i]].set(1)
+    return mask > 0
 
-    positions: Tuple[jnp.ndarray]
-
-    def to_binary_mask(self, N):
-        r"""
-        Convert sensors to binary mask
-        Args:
-            N (Tuple[int]): grid size
-
-        Returns:
-            jnp.ndarray: binary mask
-        """
-        mask = jnp.zeros(N)
-        for i in range(len(self.positions[0])):
-            mask = mask.at[self.positions[0][i], self.positions[1][i]].set(1)
-        return mask > 0
-
+  def __call__(self, u: Field):
+    r"""Returns the values of the field u at the sensors positions.
+    Args:
+      u (Field): The field to be sampled.
+    """
+    if len(self.positions) == 1:
+      return u.on_grid[self.positions[0]]
+    elif len(self.positions) == 2:
+      return u.on_grid[self.positions[0], self.positions[1]]
+    elif len(self.positions) == 3:
+      return u.on_grid[self.positions[0], self.positions[1], self.positions[2]]
+    else:
+      raise ValueError("Sensors positions must be 1, 2 or 3 dimensional. Not {}".format(
+        len(self.positions)
+      ))
 
 @dataclass
 class TimeAxis:
-    r"""Temporal vector to be used for acoustic
-    simulation based on the pseudospectral method of
-    [k-Wave](http://www.k-wave.org/)
-    Attributes:
-        dt (float): time step
-        t_end (float): simulation end time
+  r"""Temporal vector to be used for acoustic
+  simulation based on the pseudospectral method of
+  [k-Wave](http://www.k-wave.org/)
+  Attributes:
+    dt (float): time step
+    t_end (float): simulation end time
+  """
+  dt: float
+  t_end: float
+
+  def to_array(self):
+    r"""Returns the time-axis as an array"""
+    return jnp.arange(0, self.t_end, self.dt)
+
+  @staticmethod
+  def from_medium(medium: Medium, cfl: float = 0.3, t_end=None):
+    r"""Construct a `TimeAxis` object from `kGrid` and `Medium`
+    Args:
+      grid (kGrid):
+      medium (Medium):
+      cfl (float, optional):  The [CFL number](http://www.k-wave.org/). Defaults to 0.3.
+      t_end ([float], optional):  The final simulation time. If None,
+          it is automatically calculated as the time required to travel
+          from one corner of the domain to the opposite one.
     """
-    dt: float
-    t_end: float
-
-    def to_array(self):
-        r"""Returns the time-axis as an array"""
-        return jnp.arange(0, self.t_end, self.dt)
-
-    @staticmethod
-    def from_medium(medium: Medium, cfl: float = 0.3, t_end=None):
-        r"""Construct a `TimeAxis` object from `kGrid` and `Medium`
-        Args:
-            grid (kGrid):
-            medium (Medium):
-            cfl (float, optional):  The [CFL number](http://www.k-wave.org/). Defaults to 0.3.
-            t_end ([float], optional):  The final simulation time. If None,
-                    it is automatically calculated as the time required to travel
-                    from one corner of the domain to the opposite one.
-        """
-        dt = cfl * min(medium.domain.dx) / compose(medium.sound_speed)(np.max)
-        if t_end is None:
-            t_end = jnp.sqrt(
-                sum((x[-1] - x[0]) ** 2 for x in medium.domain.spatial_axis)
-            ) / compose(medium.sound_speed)(np.min)
-        return TimeAxis(dt=float(dt), t_end=float(t_end))
+    dt = cfl * min(medium.domain.dx) / functional(medium.sound_speed)(np.max)
+    if t_end is None:
+      t_end = jnp.sqrt(
+        sum((x[-1] - x[0]) ** 2 for x in medium.domain.spatial_axis)
+      ) / functional(medium.sound_speed)(np.min)
+    return TimeAxis(dt=float(dt), t_end=float(t_end))
 
 
 def _circ_mask(N, radius, centre):
-    x, y = np.mgrid[0 : N[0], 0 : N[1]]
-    dist_from_centre = np.sqrt((x - centre[0]) ** 2 + (y - centre[1]) ** 2)
-    mask = (dist_from_centre < radius).astype(int)
-    return mask
+  x, y = np.mgrid[0 : N[0], 0 : N[1]]
+  dist_from_centre = np.sqrt((x - centre[0]) ** 2 + (y - centre[1]) ** 2)
+  mask = (dist_from_centre < radius).astype(int)
+  return mask
