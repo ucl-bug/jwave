9414cf1f67e7e7ecb5abd1f7b07371fe02c48094
Mirella D'Angelo <mirelladangelo@MacBook-Pro-di-Mirella.local>
2022-03-25 11:44:22 +0100

    removed pre-jaxdf copy

--- jwave copy/__init__.py
+++
@@ -1,7 +0,0 @@
-# nopycln: file
-from jwave import acoustics as acoustics
-from jwave import geometry as geometry
-from jwave import phantoms as phantoms
-from jwave import signal_processing as signal_processing
-from jwave import utils as utils
-from jwave import version as version
