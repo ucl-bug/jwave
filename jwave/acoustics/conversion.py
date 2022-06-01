4cf4f05e7922ee3b981a658eef9f30f784324829
Antonio Stanziola <stanziola.antonio@gmail.com>
2022-06-01 14:06:46 +0100

    docstrings in conversion.py

--- jwave/acoustics/conversion.py
+++ jwave/acoustics/conversion.py
@@ -7,22 +7,36 @@ from jwave.geometry import Sensors
 def pressure_from_density(
     sensors_data: jnp.ndarray, sound_speed: jnp.ndarray, sensors: Sensors
 ) -> jnp.ndarray:
-    """
-    Calculate pressure from acoustic density given by the raw output of the
-    timestepping scheme.
+  r"""
+  Calculate pressure from acoustic density given by the raw output of the
+  timestepping scheme.
 
-    Args:
-        sensors_data: Raw output of the timestepping scheme.
-        sound_speed: Sound speed of the medium.
-        sensors: Sensors object.
+  Args:
+      sensors_data: Raw output of the timestepping scheme.
+      sound_speed: Sound speed of the medium.
+      sensors: Sensors object.
 
-    Returns:
-        jnp.ndarray: Pressure time traces at sensor locations
-    """
-    if sensors is None:
-        return jnp.sum(sensors_data[1], -1) * (sound_speed ** 2)
-    else:
-        return jnp.sum(sensors_data[1], -1) * (sound_speed[sensors.positions] ** 2)
+  Returns:
+      jnp.ndarray: Pressure time traces at sensor locations
+  """
+  if sensors is None:
+    return jnp.sum(sensors_data[1], -1) * (sound_speed ** 2)
+  else:
+    return jnp.sum(sensors_data[1], -1) * (sound_speed[sensors.positions] ** 2)
 
-def db2neper(alpha, y):
+def db2neper(
+  alpha: jnp.ndarray,
+  y: jnp.ndarray,
+):
+  r'''
+  Transforms absorption units from decibels to nepers.
+  See http://www.k-wave.org/documentation/db2neper.php
+
+  Args:
+      alpha(jnp.ndarray): Absorption coefficient in decibels.
+      y(jnp.ndarray): Distance in meters.
+
+  Returns:
+      jnp.ndarray: Absorption coefficient in nepers.
+  '''
   return 100 * alpha * ((1e-6/(2*np.pi))**y) / (20 * np.log10(np.exp(1)))
