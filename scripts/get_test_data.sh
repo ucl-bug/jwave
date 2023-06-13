e86d363da0e79f54eacc84e9547e01129b464998
STS <dev@stanziola.it>
2023-06-13 13:35:51 +0100

    combine regression and unit/integration tests coverage

--- scripts/get_test_data.sh
+++ scripts/get_test_data.sh
@@ -23,7 +23,7 @@ mkdir -p .test_data
 git clone https://github.com/ucl-bug/jwave-data.git .test_data
 
 # Moving test data to the right place
-mv .test_data/tests/* ./tests
+cp -ur .test_data/tests/* ./tests/ && rm -r .test_data/tests/*
 
 # Removing the temporary directory
 rm -rf .test_data
