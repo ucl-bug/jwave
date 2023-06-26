1ba0d5e5eb097a9cc0b8348947ab25990e778c9a
Thomas Else <56722586+tomelse@users.noreply.github.com>
2023-06-26 19:42:45 +0100

    Update tests

--- tests/test_off_grid_sensors.py
+++ tests/test_off_grid_sensors.py
@@ -2,11 +2,8 @@ from jwave.geometry import _bli_function, BLISensors, Domain, FourierSeries
 import numpy as np
 import pytest
 
-# Set a small tolerance for the bli function. This is fairly liberal to allow it to be differentiable.
-TOLERANCE = 1e-3
 
-
-# @pytest.
+@pytest.mark.parametrize("n_grid", [(100, ), (101, )])
 def test_bli_function(n_grid=100):
     # Make a load of sensors on the grid. Check the bli function.
     y = _bli_function(np.arange(n_grid), np.arange(n_grid), n_grid)
@@ -24,6 +21,7 @@ def test_bli_function(n_grid=100):
     assert (np.all(np.isclose(np.sum(y, axis=1), 1)))
 
 
+@pytest.mark.parametrize("nx,ny,nz", [(100, 100, 100), (100, 101, 102)])
 def test_sensor(nx=100, ny=100, nz=100):
     # Check that it is identical to on-grid detectors in that case.
     n_detectors = min(nx, ny, nz)
@@ -41,11 +39,11 @@ def test_sensor(nx=100, ny=100, nz=100):
 
     p = np.random.random((nx, ny, nz))
 
-    s1d = BLISensors((x, ), (nx, ))
+    s1d = BLISensors((x,), (nx,))
     s2d = BLISensors((x, y), (nx, ny))
     s3d = BLISensors((x, y, z), (nx, ny, nz))
 
-    domain1d = Domain((nx, ), (1, ))
+    domain1d = Domain((nx,), (1,))
     p1d = FourierSeries(p[:, 0, 0], domain1d)
 
     domain2d = Domain((nx, ny), (1, 1))
@@ -55,16 +53,16 @@ def test_sensor(nx=100, ny=100, nz=100):
     p3d = FourierSeries(p, domain3d)
 
     result = s1d(p1d, None, None)
-    assert(np.all(result[..., 0] == p[xi, 0, 0]))
+    assert (np.all(result[..., 0] == p[xi, 0, 0]))
 
     result = s2d(p2d, None, None)
-    assert(np.all(result[..., 0] == p[xi, yi, 0]))
+    assert (np.all(result[..., 0] == p[xi, yi, 0]))
 
     result = s3d(p3d, None, None)
-    assert(np.all(result[..., 0] == p[xi, yi, zi]))
+    assert (np.all(result[..., 0] == p[xi, yi, zi]))
 
     # Check off-grid (perturb a bit):
-    s3d = BLISensors((x+0.25, y+0.3, z+0.1), (nx, ny, nz))
+    s3d = BLISensors((x + 0.25, y + 0.3, z + 0.1), (nx, ny, nz))
     domain3d = Domain((nx, ny, nz), (1, 1, 1))
     # Check ones in ones out.
     p3d = FourierSeries(np.ones((nx, ny, nz)), domain3d)
